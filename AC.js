1c389a11506e20e4146c51e462cce5cea9707c13
TP1703 <85887100+Thanujaa6BC@users.noreply.github.com>
2023-05-24 16:33:56 +0530

    Add files via upload

---
+++ AC.js
@@ -0,0 +1,31 @@
+Status = "";
+AC_image = "";
+
+function preload() {
+    AC_image = loadImage("AC.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Status").innerHTML = "Statuss : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    Status = true;
+    object_detector.Detect(AC_image, gotResults);
+
+}
+
+function gotResults(results, error) {
+ if(error) {
+    console.log(error);
+ }
+ console.log(results);
+}
+
+function draw() {
+    image(AC_image,0,0);
+}
---
+++ tv.js
@@ -0,0 +1,31 @@
+Status = "";
+TV_image = "";
+
+function preload() {
+    TV_image = loadImage("TV.jpeg");
+}
+
+function setup() {
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Status").innerHTML = "Statuss : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    Status = true;
+    object_detector.Detect(TV_image, gotResults);
+
+}
+
+function gotResults(results, error) {
+ if(error) {
+    console.log(error);
+ }
+ console.log(results);
+}
+
+function draw() {
+    image(TV_image,0,0);
+}
